924aa4e747f22cdca9e8a930cd4ef4f87d99a217
zxfjd3g <258147149@qq.com>
2017-04-12 10:20:15 +0800

    update

---
+++ work/webpack_demo2/src/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <img src="./assets/logo.png" alt="logo">
+    <hello></hello>
+  </div>
+</template>
+<script>
+  import hello from './components/hello'
+  export default {
+    components: {
+      hello
+    }
+  }
+</script>
+<style>
+  img {
+    width: 100px;
+    height: 100px;
+  }
+</style>
---
+++ work/webpack_demo2/src/components/hello.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+      <p>{{msg}}2222</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: '尚硅谷2222333'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  p {
+    color: blue;
+  }
+</style>
